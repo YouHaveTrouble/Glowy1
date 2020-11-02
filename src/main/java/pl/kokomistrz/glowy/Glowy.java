b9a2ca053cfe7b6b2869082370e2c39a790a4a19
kokomistrz <46074179+kokomistrz@users.noreply.github.com>
2020-11-02 09:53:28 +0100

    Utworzyłem Plugin

---
+++ src/main/java/pl/kokomistrz/glowy/Glowy.java
@@ -0,0 +1,17 @@
+package pl.kokomistrz.glowy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Glowy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
